f3728ec4fde30ca20f9fd84192f1151de0ba0272
chittaranjanpanda <chittaranjan@hotmail.com>
2021-06-26 01:30:11 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/JavaSdpApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSdpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSdpApplication.class, args);
+	}
+
+}
